dac674cc05cea3db2a0cdaf76e47ea2eb8309d94
Nemobii <nicolas-walker@bluewin.ch>
2024-09-28 16:03:42 +0200

    fixing github actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
-/**
-* @type {import('next').NextConfig}
-*/
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  images: {
-    loader: "akamai",
-    path: "",
-  },
-  assetPrefix:'https://nemobii.github.io/portfolio-page/',
-  reactStrictMode: true,
+  reactStrictMode: true
 };
 
 export default nextConfig;
