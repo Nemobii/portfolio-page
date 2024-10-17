5d99770ea31af10b45158bffdaab2ac251604f38
Nemobii <nicolas-walker@bluewin.ch>
2024-10-17 20:03:26 +0200

    fix images in pages

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import Lightbox from 'yet-another-react-lightbox';
+import Image from 'next/image'
 
 interface GalleryProps {
     images: string[];
@@ -16,9 +17,11 @@ const Gallery: React.FC<GalleryProps> = ({ images }) => {
             <div className="grid grid-cols-3 gap-4">
                 {images.map((image, idx) => (
                     <div key={image} className="overflow-hidden hover:-translate-y-2 transition-all duration-300">
-                        <img
+                        <Image
                             src={image}
                             alt={`Image ${idx + 1}`}
+                            width={860}
+                            height={860}
                             className="w-full h-auto cursor-pointer aspect-[1/1] object-cover"
                             onClick={() => setIndex(idx)}
                         />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,12 @@ import Gallery from "./components/Gallery";
 import Layout from "./components/Layout";
 import { getImages } from "@/app/utils/imageUtils";
 import React from 'react';
-import Quote from "./components/Quote";
 
 const Home: React.FC = () => {
   const images = getImages("images/Animals");
 
   return (
     <Layout>
-      <Quote />
       <div className="max-w-[1200px] mx-auto">
         <Gallery images={images} />
       </div>
